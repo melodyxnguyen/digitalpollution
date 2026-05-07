14b5109cb76965fc1e30dbd8c96ff1afb7de86f9
Melody Nguyen <melodyxnguyen@gmail.com>
2026-05-07 10:33:44 -0400

    landing photo

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from 'react'
 import { Link } from 'react-router-dom'
-import nightGif from '../../images/night.gif'
+import nightGif from '../../images/mitdatacenter.webp'
 import './Landing.css'
 
 const stats = [
